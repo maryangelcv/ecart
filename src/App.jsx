23f5e4c3e582810d4f265b432e00a68eed991ceb
maryangelcv <maryangelcv2@gmail.com>
2025-01-07 13:49:19 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import View from './pages/View'
+import Wishlist from './pages/Wishlist'
+import Pnf from './pages/Pnf'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+           <Route path='/' element={<Home/>}/>
+           <Route path='/wishlist' element={<Wishlist/>}/>
+           <Route path='/cart' element={<Cart/>}/>
+           <Route path='/:id/view' element={<View/>}/>
+           <Route path='/*' element={<Pnf/>}/>
+    </Routes>
+
+    {/* footer */}
+    <Footer/>
+    </>
+  )
+}
+
+export default App
